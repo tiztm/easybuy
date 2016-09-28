36fb3c2e6cc487a9c674cc18252997736ab20152
nilszhang@gmail.com <nilszhang@gmail.com>
2016-09-28 17:40:18 +0800

    easybuy

---
+++ src/main/src/controller/loginServlet.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IDEA
+ * User:    tiztm
+ * Date:    2016/9/27.
+ */
+@WebServlet(name="loginServlet", urlPatterns="/servlet/example")
+public class loginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Hello User1.");
+    }
+
+}
